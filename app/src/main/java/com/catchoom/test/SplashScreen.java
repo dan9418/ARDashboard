15c4cc2e903f07b151c1bbd35b7bb818d5ed504d
Dan <dpb34@pitt.edu>
2017-02-24 10:25:16 -0500

    Added base project functionality to ImageRecognitionActivity.

--- app/src/main/java/com/catchoom/test/SplashScreen.java
+++ app/src/main/java/com/catchoom/test/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.catchoom.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                launchCamera(v);
+            }
+        });
+
+    }
+
+    private void launchCamera(View view) {
+        Intent intent = new Intent(this, ImageRecognitionActivity.class);
+        startActivity(intent);
     }
 }
