6547de4c93e0f9a1c22c676bf3789c8627f8d24b
Dan <dpb34@pitt.edu>
2017-04-14 16:16:08 -0400

    Updated the help page.

--- app/src/main/java/com/catchoom/test/HelpActivity.java
+++ app/src/main/java/com/catchoom/test/HelpActivity.java
@@ -2,6 +2,8 @@ package com.catchoom.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        final Button button_back = (Button) findViewById(R.id.back_button);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goBack(v);
+            }
+        });
     }
+
+    private void goBack(View view) {
+        finish();
+    }
+
 }
